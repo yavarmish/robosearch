5019588c0657529eda5e1010273d62b22dbc65ac
Yash Mishra <yashmishra.ic18@nsut.ac.in>
2022-05-23 23:08:40 +0530

    bug fixes

--- src/Components/ErrorBoundaries.js
+++ src/Components/ErrorBoundaries.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-class ErrorBoundary extends React{
+class ErrorBoundary extends React.Component{
     constructor(props){
         super(props);
         this.state={
